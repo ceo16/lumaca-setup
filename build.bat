a6bba11d1db91d8ac00ea0bfa72a76dd55917222
ceo16 <ceo16@libero.it>
2023-12-27 16:02:33 +0100

    Update build.bat

--- build.bat
+++ build.bat
@@ -12,7 +12,7 @@ to set the default configuration and to build the setup from sources.
 
 :: ---- BUILDER OPTION ----
 
-set retrobat_version=6.0.0
+set lumaca_version=1.0.0
 set retroarch_version=1.16.0
 
 set get_batgui=0
@@ -24,7 +24,7 @@ set get_emulationstation=1
 set get_emulators=0
 set get_lrcores=1
 set get_retroarch=1
-set get_retrobat_binaries=1
+set get_lumaca_binaries=1
 set get_system=1
 set get_wiimotegun=1
 
@@ -42,7 +42,7 @@ set setup_compiler=ISCC
 
 set deps_list=(git !setup_compiler! 7za wget curl)
 set submodules_list=(bios decorations system)
-set packages_list=(retrobat_binaries batgui emulationstation default_theme batocera_ports mega_bezels retroarch roms wiimotegun)
+set packages_list=(lumaca_binaries batgui emulationstation default_theme batocera_ports mega_bezels retroarch roms wiimotegun)
 set legacy_cores_list=(4do emuscv hatarib imageviewer mame2014 mame2016)
 set emulators_black_list=(3dsen pico8 retroarch ryujinx steam teknoparrot yuzu yuzu-early-access)
 
